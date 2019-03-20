3815ee3a33f872b77b647e8422e3e3d210fbdd29
Michael Miles <michael.gage.miles@gmail.com>
2019-03-19 19:11:03 -0700

    Conformed to MVC protocol.

---
+++ BitcoinConversion/Controller/SelectorViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SelectorViewController.swift
+//  BitcoinConversion
+//
+//  Created by Michael Miles on 3/19/19.
+//  Copyright © 2019 Michael Miles. All rights reserved.
+//
+
+import UIKit
+
+class SelectorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
