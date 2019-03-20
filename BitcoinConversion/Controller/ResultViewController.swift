c0d4c4b53a4b6c436554ec72b16f251f4d558de2
Michael Miles <michael.gage.miles@gmail.com>
2019-03-19 21:33:48 -0700

    Set up segue to results.

---
+++ BitcoinConversion/Controller/ResultViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ResultViewController.swift
+//  BitcoinConversion
+//
+//  Created by Michael Miles on 3/19/19.
+//  Copyright © 2019 Michael Miles. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    
+    var currencyResult = ""
+    var symbolResult = ""
+    var searchURL = ""
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- BitcoinConversion/Controller/SelectorViewController.swift
+++ BitcoinConversion/Controller/SelectorViewController.swift
@@ -8,23 +8,52 @@
 
 import UIKit
 
-class SelectorViewController: UIViewController {
-
+class SelectorViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    let baseURL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC"
+    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+    let symbolArray = ["$", "R$", "$", "¥", "€", "£", "$", "Rp", "₪", "₹", "¥", "$", "kr", "$", "zł", "lei", "₽", "kr", "$", "$", "R"]
+    var finalURL = ""
+    var currencySelected = ""
+    var symbolSelected = ""
+    
+    @IBOutlet weak var helloLabel: UILabel!
+    @IBOutlet weak var currencyPicker: UIPickerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        currencyPicker.delegate = self
+        currencyPicker.dataSource = self
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //Mark: Picker Functions
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
     }
-    */
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return currencyArray.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return currencyArray[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        currencySelected = currencyArray[row]
+        
+        finalURL = baseURL + currencySelected
+        symbolSelected = symbolArray[row]
+    }
+
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destinationVC = segue.destination as! ResultViewController
+        
+        destinationVC.currencyResult = currencySelected
+        destinationVC.searchURL = finalURL
+        destinationVC.symbolResult = symbolSelected
+    }
 
 }
