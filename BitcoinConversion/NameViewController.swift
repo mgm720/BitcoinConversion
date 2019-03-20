85e684e43a9dc72864ede66bedbdc927bbaa773c
Michael Miles <michael.gage.miles@gmail.com>
2019-03-19 18:27:13 -0700

    Fixed bug that caused error with keyboard on smaller phones

--- BitcoinConversion/NameViewController.swift
+++ BitcoinConversion/NameViewController.swift
@@ -17,10 +17,28 @@ class NameViewController: UIViewController {
         super.viewDidLoad()
         
         setupUI()
+        
+        //makes app compatible with smaller screen iOS devices
+        if self.view.frame.height < 600 {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+        }
     }
 
     @IBAction func goButtonPressed(_ sender: Any) {
         
+    }
+    
+    @objc func keyboardWillShow(notification: NSNotification) {
+            if self.view.frame.origin.y == 0 {
+                self.view.frame.origin.y -= 100
+            }
+    }
+    
+    @objc func keyboardWillHide(notification: NSNotification) {
+        if self.view.frame.origin.y != 0 {
+            self.view.frame.origin.y = 0
+        }
     }
     
     func setupUI() {
