e447ca81ddc11b2f0b9ee8a1cdea9f8d6bf611d0
Michael Miles <michael.gage.miles@gmail.com>
2019-03-19 22:03:23 -0700

    Installed Alamofire and SwiftyJSON, and added networking calls.

--- BitcoinConversion/Controller/ResultViewController.swift
+++ BitcoinConversion/Controller/ResultViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class ResultViewController: UIViewController {
     
@@ -14,11 +16,52 @@ class ResultViewController: UIViewController {
     var symbolResult = ""
     var searchURL = ""
 
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
+        getBitcoinData(url: searchURL)
     }
     
+    //
+    //    //MARK: - Networking
+    //    /***************************************************************/
+    //
+    func getBitcoinData(url: String) {
+        
+        Alamofire.request(url, method: .get)
+            .responseJSON { response in
+                if response.result.isSuccess {
+                    
+                    print("Sucess! Got the Bitcoin data")
+                    let bitcoinJSON : JSON = JSON(response.result.value!)
+                    
+                    self.updateBitcoinData(json: bitcoinJSON)
+                    
+                } else {
+                    print("Error: \(String(describing: response.result.error))")
+                    self.resultLabel.text = "Connection Issues"
+                }
+        }
+        
+    }
+    //
+    //
+    //
+    //
+    //
+    //    //MARK: - JSON Parsing
+    //    /***************************************************************/
+    //
+    func updateBitcoinData(json : JSON) {
+        
+        if let priceResult = json["ask"].double {
+            resultLabel.text = "\(symbolResult)\(priceResult) \(currencyResult)"
+        }
+        else {
+            resultLabel.text = "Price Unavailable"
+        }
+    }
 
 }
